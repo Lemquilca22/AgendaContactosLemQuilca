17b2e78be0393ac59bab20c8217b33901f9c4038
Lemquilca22 <lemquilca22@gmail.com>
2025-09-26 09:55:24 +0200

    Agenda contacto
    ....

--- Agenda contactos/src/Main.java
+++ Agenda contactos/src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scan = new Scanner(System.in);
+        //Implementar un menu con 4 opciones
 
-    }
-}
+        System.out.println("¿Que quiere hacer hoy?"+ " Selecciona una opcion");
+        System.out.println("Añadir contacto"+"\n"+"Mostrar contactos"+"\n"+"Buscar contacto"+"\n"+"Salir");
+
+        String leer = scan.nextLine();
+
+        System.out.println("Usted a escogido "+leer);
+
+        }
+        }
+
