6854356c8d1136eb877870565daddf643e230adb
Lemsito22 <lemquilca22@gmail.com>
2025-11-28 11:37:16 +0100

    ...

--- Agenda contactos/src/Main2.java
+++ Agenda contactos/src/Main2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main2 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         //Implementar un menu con 4 opciones
         boolean salir = true;
         String rpta;
@@ -13,46 +13,60 @@ public class Main2 {
             System.out.println("¿Que quiere hacer hoy?"+ " Selecciona una opcion");
             System.out.println("1) Añadir contacto"+"\n"+"2) Mostrar contactos"+"\n"+"3) Modificar contacto");
            //+"\n"+"4)Salir"
-            int opcion = scan.nextInt();
+            int opcion = sc.nextInt();
             int num;
             String nom;
             switch (opcion) {
                 case 1:
                     System.out.println("Ingrese el numero de telefono: ");
-                    arraynumero[numerocontactoañadidos]=scan.nextInt();
+                    arraynumero[numerocontactoañadidos]=sc.nextInt();
+                    sc.nextLine();
                     System.out.println("Ingrese el nombre que le quiere asignar: ");
-                    scan.next();
-                    arraycontacto[numerocontactoañadidos]=scan.nextLine();
+                    arraycontacto[numerocontactoañadidos]=sc.nextLine();
                     numerocontactoañadidos++;
                     break;
                 case 2:
-                    System.out.println("Esta es tu lista de contactos");
-                    for (int i = 0; i < arraycontacto.length; i++) {
-                        if (arraycontacto[numerocontactoañadidos]== null) {
+                    System.out.println("Esta es tu lista de contactos: ");
+                    for (int i = 0; i < 100; i++) {
+                        if (arraycontacto[i]== null) {
                             continue;
                         } else {
-                            System.out.println("Nombre: "+arraycontacto[numerocontactoañadidos]+"\nNumero: "+arraynumero[numerocontactoañadidos]);
+                            System.out.println((i)+") Nombre: "+arraycontacto[i]+" Numero: "+arraynumero[i]);
                         }
                     }
 
                     break;
                 case 3:
                     System.out.println("Que contacto quieres modificar?");
-                    nom = scan.next();
-                    System.out.println("Perfecto, encontré a "+nom);
-                    System.out.println("cual deseas que sea el nuevo nombre");
-                    nom = scan.next();
-                    System.out.println("Haz modificado el contacto a "+nom);
+                    for (int i = 0; i < 100; i++) {
+                        if (arraycontacto[i]== null) {
+                            continue;
+                        } else {
+                            System.out.println((i)+") Nombre: "+arraycontacto[i]+" Numero: "+arraynumero[i]);
+                        }
+                    }
+                    int modificar = sc.nextInt();
+                    System.out.println("Perfecto, encontré a "+arraycontacto[modificar]);
+                    System.out.println("¿Quieres modificar el numero o el nombre?");
+                    String respuesta=sc.next();
+                    if (respuesta.equalsIgnoreCase("nombre")) {
+                        System.out.println("cual deseas que sea el nuevo nombre?");
+                        String nuevoNombre=sc.next();
+                        arraycontacto[modificar]=nuevoNombre;
+                        System.out.println("Haz modificado el contacto a "+nuevoNombre);
+                    } else if (respuesta.equalsIgnoreCase("numero")) {
+                        System.out.println("cual deseas que sea el nuevo numero?");
+                        int nuevonumero=sc.nextInt();
+                        arraynumero[modificar]=nuevonumero;
+                        System.out.println("Haz modificado el contacto a "+nuevonumero);
+                    }
 
-                   break;
-//                case 4:
-//                    System.out.println("Nos vemos luego");
-//
-//                    break;
+
+                    break;
             }
 //            scan.next();
             System.out.println("Deseas realizar otra operación (SI/NO)");
-            rpta= scan.next();
+            rpta= sc.next();
             if (rpta.equalsIgnoreCase("NO")){
                 salir=false;
                 System.out.println("Gracias, nos vemos pronto ");
